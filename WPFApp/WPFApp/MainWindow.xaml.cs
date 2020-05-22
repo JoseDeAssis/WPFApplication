b0f53ac17803fe3ab8203f7043c9047215678129
Jose Maria <jose.assis@aluno.uece.br>
2020-05-22 12:26:14 -0300

    Aula 1 Raian sobre Floating Action Button

---
+++ WPFApp/WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFApp.Themes;
+
+namespace WPFApp {
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            FloatingActionButton.Click += new RoutedEventHandler(ShowMsg);
+        }
+
+        private void ShowMsg(object sender, RoutedEventArgs e) {
+            MessageBox.Show("This is a FAB");
+        }
+    }
+}
---
+++ WPFApp/WPFApp/Themes/FloatingActionButton.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp.Themes {
+    public class FloatingActionButton : Button {
+        private static readonly string PART_ELLIPSE = "PART_Ellipse";
+        private static readonly string PART_BODY = "PART_Body";
+
+        private Ellipse _ellipse;
+        private Grid _bodyGrid;
+
+        static FloatingActionButton() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FloatingActionButton), new FrameworkPropertyMetadata(typeof(FloatingActionButton)));
+        }
+
+        public Brush HighlightColor {
+            get { return (Brush)GetValue(HighlightColorProperty); }
+            set { SetValue(HighlightColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HighlightColorProperty =
+            DependencyProperty.Register(nameof(HighlightColor), typeof(Brush), typeof(FloatingActionButton), new PropertyMetadata(Brushes.Red));
+
+
+        public override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+
+            if (this.Template != null) {
+                _ellipse = GetTemplateChild<Ellipse>(PART_ELLIPSE);
+                _bodyGrid = GetTemplateChild<Grid>(PART_BODY);
+
+                if (_ellipse != null) {
+                    _ellipse.Fill = Brushes.Crimson;
+                    _bodyGrid.MouseEnter += (sender, e) => {
+                        _ellipse.Fill = Brushes.Aqua;
+                    };
+
+                    _bodyGrid.MouseLeave += (sender, e) => {
+                        _ellipse.Fill = Brushes.Crimson;
+                    };
+                }
+
+            }
+        }
+
+        T GetTemplateChild<T>(string name) where T : DependencyObject {
+            var child = GetTemplateChild(name) as T;
+
+            if (child is null) {
+                throw new NullReferenceException(name);
+            }
+
+            return child;
+        }
+    }
+}
